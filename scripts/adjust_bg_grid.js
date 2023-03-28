6201bfc12b07ff3e287b67b03e8670a1eb62a747
K4G13 <polcyn.piotrek@gmail.com>
2023-03-29 00:11:32 +0200

    version 6.1

--- scripts/adjust_bg_grid.js
+++
@@ -1,7 +0,0 @@
-var grid_unit = window.getComputedStyle(document.body).getPropertyValue('--unit');
-grid_unit = grid_unit.substring(1,grid_unit.length-2)
-if(!grid_unit) grid_unit = 50
-if($('body').hasClass('dotted')) $('body').css('background-size',`${grid_unit}px ${grid_unit}px`)
-var grid_offset_x = (window.innerWidth%grid_unit)/2
-var grid_offset_y = (window.innerHeight%grid_unit)/2
-if($('body').hasClass('dotted')) $('body').css('background-position',`${grid_offset_x}px ${grid_offset_y}px`)
---
+++ scripts/save_localy.js
@@ -0,0 +1,69 @@
+var DB
+
+function saveLocaly(){
+
+    var data = { html: $('#container').html() }
+    var json = JSON.stringify(data);    
+    localStorage.setItem('data', json);
+
+}
+
+function loadFromLocal(){
+
+    var data = JSON.parse(localStorage.getItem('data'))
+    // console.log(data)
+
+    var defaultData = { html: "\n\n        <button id=\"add-btt\">+</button>\n      \n        <a href=\"https://www.youtube.com/\" target=\"_blank\" title=\"youtube\">youtube</a>\n        <a href=\"https://www.facebook.com/\" target=\"_blank\" title=\"facebook\">facebook</a>\n        <a href=\"https://www.twitch.tv/directory/following\" target=\"_blank\" title=\"twitch\">twitch</a>\n        <a href=\"https://www.netflix.com/\" target=\"_blank\" title=\"netflix\">netflix</a>\n        <a href=\"https://mail.google.com/mail/u/0/#inbox\" target=\"_blank\" title=\"gmail\">gmail</a>\n        <a href=\"https://glovoapp.com/pl/pl/poznan/\" target=\"_blank\" title=\"glovo\">glovo</a>\n\n    "}
+
+    if( !data ) {
+        console.log("LOCALSTORAGE ERROR - loading default data")
+        data = defaultData
+    }
+
+    $('#container').html(data.html)
+
+    $('#add-btt').click(()=>add_new_tile())
+    $('#container a').mousedown(function(event){ 
+        if( event.button == 2 ) { 
+            open_contextmenu(this,event)
+            return false; 
+        }
+        return true; 
+    }); 
+
+    for( let i=0; i<$('#container a').length; i++ ){
+        let el = $('#container a')[i]
+        el = $(el)
+        let url = el.attr("href")
+        let title = el.attr("title")
+        el.html("")
+        if(      url.indexOf('youtube') >= 0 )  el.get(0).style.backgroundImage = "url(resorces/icons/youtube.png)"
+        else if( url.indexOf('facebook') >= 0 ) el.get(0).style.backgroundImage = "url(resorces/icons/facebook.png)"
+        else if( url.indexOf('glovo') >= 0 )    el.get(0).style.backgroundImage = "url(resorces/icons/glovo.png)"
+        else if( url.indexOf('mail') >= 0 )     el.get(0).style.backgroundImage = "url(resorces/icons/gmail.png)"
+        else if( url.indexOf('netflix') >= 0 )  el.get(0).style.backgroundImage = "url(resorces/icons/netflix.png)"
+        else if( url.indexOf('twitch') >= 0 )   el.get(0).style.backgroundImage = "url(resorces/icons/twitch.png)"
+        else if( url.indexOf('instagram') >= 0 )el.get(0).style.backgroundImage = "url(resorces/icons/instagram.png)"
+        else if( url.indexOf('messenger') >= 0 )el.get(0).style.backgroundImage = "url(resorces/icons/messenger.png)"
+        else if( url.indexOf('outlook') >= 0 )  el.get(0).style.backgroundImage = "url(resorces/icons/outlook.png)"
+        else{
+            el.get(0).style.backgroundImage = "none"
+            if( title.split(' ').length >= 3 ){
+                var short = title.split(' ')[0][0].toUpperCase() + title.split(' ')[1][0].toUpperCase() + title.split(' ')[2][0].toUpperCase()
+            }
+            else if( title.split(' ').length == 2 ) {
+                var short = title.split(' ')[0][0].toUpperCase() + title.split(' ')[0][1].toLowerCase() + title.split(' ')[1][0].toUpperCase()
+            }
+            else if(title.length>=3){
+                var short = title[0].toUpperCase() + title[1].toLowerCase() + title[2].toLowerCase()
+            }
+            else{
+                var short = title
+            }
+        }
+        el.html(short)
+    }
+
+}
+
+loadFromLocal()
